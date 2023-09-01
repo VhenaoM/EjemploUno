931743c55a7d8429f8367b7cfc377fcf5cda9778
VhenaoM <vic.henao85@gmail.com>
2023-08-31 23:09:41 -0500

    1er algoritmo

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,40 +1,42 @@
-﻿
+﻿//1ro definir el nombre de mi clase//
 
-// 1ro definir el nombre de la clase//
-public class EjemploUno  
+public class EjemploUno
 
 {
     static void Main(string[] args)
     {
-      // ingresar por pantala 2 números y calcuar las 4 operaciones arimeticascon cada numero
-     //imprimir en pantalla elresultado de cada opeación//
 
-        // declaracin de mis variables (datos de etrada//
+        //datos de entrada//
+
         int num1, num2, suma, resta, producto, cociente;
-        // 3ro pedir los valores por pantalla (consola)
 
-        Console.Write("Ingrese su primer numero: ");
+        //3ro pedir valores de pantalla//
+
+        Console.WriteLine("Ingrese su primer número ");
         num1 = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("Ingrese su segundo numero: ");
+        Console.WriteLine("Ingrese su segundo número ");
         num2 = Convert.ToInt32(Console.ReadLine());
 
-        // 4to cálculos//
-         
-       suma = num1 + num2;  
-        resta = num1 - num2;    
-        producto = num1 * num2; 
-        cociente = num1 / num2;
+        //calculos//
 
-        // 5to imprimir los resultados//
-        Console.Write("La suma es: " + suma);
+        suma = num1 + num2;
+        resta = num1 + num2;
+        producto = num1 + num2;
+        cociente = num1 + num2;
+
+        // datos de salida//
+
+        Console.Write("La suma es : + suma");
         Console.Write("\n");
-        Console.Write("la resta es: " + resta);
+        Console.Write("La resta es : + resta");
         Console.Write("\n");
-        Console.Write("El producto es: " + producto);
+        Console.Write("El producto es : + producto");    
         Console.Write("\n");
-        Console.Write("El cociente es: " + cociente);
-        
+        Console.Write("El cociente es : + cociente");
+
+
+
+
     }
-   
-}
+} 
